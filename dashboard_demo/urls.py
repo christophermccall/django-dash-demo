dd3e08ea0b72690e594f9252e8366211b578879e
Shaily Modi <shailykunal@gmail.com>
2025-02-16 17:20:34 -0500

    added Signup page, login and logout

--- dashboard_demo/urls.py
+++ dashboard_demo/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from dashboard import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.SignupPage,name='signup'),
+    path('login/',views.LoginPage,name='login'),
+    path('home/',views.HomePage,name='home'),
+    path('logout/',views.LogoutPage,name='logout'),
     path('dashboard/', include('dashboard.urls')),
 ]
