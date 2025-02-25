7538a81a818432309d69adc8995f35b585501259
Shaily Modi <shailykunal@gmail.com>
2025-02-25 09:56:50 -0500

    fixed login, logout and created pop-up notification.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import dashboard_view
 from .views import get_logins_per_day
+from dashboard.views import index
 
 urlpatterns = [
-    # path('', dashboard_view, name='dashboard'),
+    path('', dashboard_view, name='dashboard'),
     path('api/logins-per-day/', get_logins_per_day, name='logins_per_day')
 ]
--- dashboard_demo/settings.py
+++ dashboard_demo/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'dashboard_demo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,8 +115,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -188,3 +190,13 @@ if not DEBUG:
     LOGGING['loggers']['django']['level'] = 'ERROR'  # Only log errors in production
     LOGGING['loggers']['user_activity']['level'] = 'WARNING'  # Log warnings and errors for user activity
     LOGGING['handlers']['console']['level'] = 'ERROR'  # Only show errors in console for production
+
+# Messages configuration 
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.DEBUG: 'debug',
+    messages.INFO: 'info',
+    messages.SUCCESS: 'success',
+    messages.WARNING: 'warning',
+    messages.ERROR: 'danger',
+}
