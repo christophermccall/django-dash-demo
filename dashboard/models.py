dbd1e0daed2c68b30acfbfa0cadc6560ed7f3406
Nightingale <93238522+CITATS928@users.noreply.github.com>
2025-02-28 23:30:33 -0500

    profile model

--- dashboard/models.py
+++ dashboard/models.py
@@ -10,3 +10,19 @@ class UserActivity(models.Model):
 
     def __str__(self):
         return f"User {self.user.username} performed {self.action} at {self.timestamp}"
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100)
+    company_name = models.CharField(max_length=100, blank=True, null=True)
+    ORGANIZATION_CHOICES = [
+        ('for_profit', 'For-Profit'),
+        ('non_profit', 'Non-Profit'),
+    ]
+    organization_type = models.CharField(max_length=20, choices=ORGANIZATION_CHOICES, default='for_profit')
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.full_name or self.user.username
